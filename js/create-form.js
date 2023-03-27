fbf853c778bd79b98ac61877c9e4bddfc98c85fb
DavidJKTofan <dtofan@learning.eoi.es>
2023-03-27 18:20:10 +0200

    Update create-form.js

--- js/create-form.js
+++ js/create-form.js
@@ -41,7 +41,7 @@ form.addEventListener("submit", async (event) => {
       body: JSON.stringify({ url }),
     });
     console.log(response);
-    const jsons = await response.json()
+    const jsons = await response.json();
     console.log(jsons);
     // Check Status Code OK
     if (!response.ok) {
@@ -50,81 +50,78 @@ form.addEventListener("submit", async (event) => {
       return;
     }
     // Return HTML
-    resultDiv.innerHTML = "Finalising... <br>";
+    resultDiv.innerHTML = "<p>Getting the data...</p>";
     resultDiv.innerHTML += `<p>Response Status: ${response.status}</p>`;
-    resultDiv.innerHTML += `<p>JSON: ${jsons}</p>`;
   } catch (error) {
-    resultDiv.innerHTML = `ERROR... ${error}...`;
+    resultDiv.innerHTML = `ERROR when fetching data... ${error}...`;
   }
-  // // UNCOMMENT LATER
-  // // Cloudflare API call
-  // // Create Zero Trust List
-  // try {
-  //   const workers_url = `https://cloudflare-api.cf-testing.workers.dev/?identifier=${identifier}`;
-  //   const response = await fetch(workers_url, {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "Access-Control-Allow-Origin": "*",
-  //       "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-  //       "Access-Control-Allow-Headers": "*",
-  //     },
-  //     body: JSON.stringify({
-  //       // include the body data here
-  //       email: `${email}`,
-  //       authorization: `${token}`,
-  //       accountid: `${identifier}`,
-  //       data: JSON.stringify({
-  //         description: "This is a test here",
-  //         items: [{ value: "example.com" }],
-  //         name: `${lastPart}`,
-  //         type: `${list_type}`,
-  //       }),
-  //     }),
-  //   });
-  //   console.log("RESPONSE", response);
-  //   const json = JSON.stringify(response);
-  //   console.log("JSON", json);
+  // Cloudflare API call
+  // Create Zero Trust List
+  try {
+    const workers_url = `https://cloudflare-api.cf-testing.workers.dev/?identifier=${identifier}`;
+    const response = await fetch(workers_url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "*",
+      },
+      body: JSON.stringify({
+        // include the body data here
+        email: `${email}`,
+        authorization: `${token}`,
+        accountid: `${identifier}`,
+        data: JSON.stringify({
+          description: "This is a test here",
+          items: [{ "value": "example.com" }],
+          name: `${lastPart}`,
+          type: `${list_type}`,
+        }),
+      }),
+    });
+    console.log("RESPONSE", response);
+    const json = JSON.stringify(response);
+    console.log("JSON", json);
 
-  //   // BLOCKED BY CORS !!!!
-  //   // const gateway_url = `https://api.cloudflare.com/client/v4/accounts/${identifier}/gateway/lists`;
-  //   // const options = {
-  //   //   method: "POST",
-  //   //   headers: {
-  //   //     "Content-Type": "application/json",
-  //   //     "X-Auth-Email": `${email}`,
-  //   //     // "X-Auth-Key": `${token}`, // Get Global API key (legacy)
-  //   //     "Authorization": `Bearer ${token}`,
-  //   //     "Access-Control-Allow-Origin": "*",
-  //   //     "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-  //   //     "Access-Control-Allow-Headers": "*",
-  //   //   },
-  //   //   // body: `{"description":"This is a test here","items":[{"value":"example.com"}],"name":"${lastPart}","type":"${list_type}"}`,
-  //   //   body: JSON.stringify({
-  //   //     description: "This is a test here",
-  //   //     items: [{ value: "example.com" }],
-  //   //     name: `${lastPart}`,
-  //   //     type: `${list_type}`,
-  //   //   }),
-  //   // };
-  //   // console.log("OPTIONS: ", options);
-  //   // const response = await fetch(gateway_url, options);
-  //   // console.log("RESPONSE: ", response);
-  //   // const json = await response.json();
-  //   // console.log("JSON: ", json);
+    //   // BLOCKED BY CORS !!!!
+    //   // const gateway_url = `https://api.cloudflare.com/client/v4/accounts/${identifier}/gateway/lists`;
+    //   // const options = {
+    //   //   method: "POST",
+    //   //   headers: {
+    //   //     "Content-Type": "application/json",
+    //   //     "X-Auth-Email": `${email}`,
+    //   //     // "X-Auth-Key": `${token}`, // Get Global API key (legacy)
+    //   //     "Authorization": `Bearer ${token}`,
+    //   //     "Access-Control-Allow-Origin": "*",
+    //   //     "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    //   //     "Access-Control-Allow-Headers": "*",
+    //   //   },
+    //   //   // body: `{"description":"This is a test here","items":[{"value":"example.com"}],"name":"${lastPart}","type":"${list_type}"}`,
+    //   //   body: JSON.stringify({
+    //   //     description: "This is a test here",
+    //   //     items: [{ value: "example.com" }],
+    //   //     name: `${lastPart}`,
+    //   //     type: `${list_type}`,
+    //   //   }),
+    //   // };
+    //   // console.log("OPTIONS: ", options);
+    //   // const response = await fetch(gateway_url, options);
+    //   // console.log("RESPONSE: ", response);
+    //   // const json = await response.json();
+    //   // console.log("JSON: ", json);
 
-  //   if (!response.ok) {
-  //     resultDiv.innerHTML = `Error: ${response.status} ${response.statusText}`;
-  //     return;
-  //   } else {
-  //     resultDiv.innerHTML = `List ${lastPart} created...`;
-  //     var html_to_insert = `Response: ${response}`;
-  //     resultDiv.innerHTML += html_to_insert;
-  //   }
-  // } catch (error) {
-  //   console.log("Failed...");
-  //   resultDiv.innerHTML = `ERROR... ${error}...`;
-  // }
+    if (!response.ok) {
+      resultDiv.innerHTML = `Error: ${response.status} ${response.statusText}`;
+      return;
+    } else {
+      resultDiv.innerHTML += `<p>Creating List ${lastPart}...</p>`;
+      resultDiv.innerHTML += `<p>Response Status: ${response.status}</p>`;
+    }
+  } catch (error) {
+    console.log("Failed...");
+    resultDiv.innerHTML = `ERROR when creating list... ${error}...`;
+  }
 });
 
 function isValidUrl(url) {
